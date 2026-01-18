56e706777a11359077fadf395f9e81fe2ecc04a7
Mina <lunamina246@gmail.com>
2026-01-18 13:16:32 -0500

    implement Hero scroll animation

--- cocktail-project/src/components/Hero.tsx
+++ cocktail-project/src/components/Hero.tsx
@@ -1,8 +1,17 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react'
-import {SplitText} from 'gsap/all'
+import { SplitText, ScrollTrigger } from 'gsap/all'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  // If you want to add responsive behavior later, you can re-enable this:
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText('.title', {type: 'chars, words'});
     const paraSlit = new SplitText('.subtitle', {type: 'lines'});
@@ -36,9 +45,33 @@ export default function Hero() {
     .to('.right-leaf', {
       y: 200
     }, 0)
-    .to('.left-leaf', {y: -200}, 0)
+    .to('.left-leaf', {y: -200}, 0);
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    // Video scroll animation
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    // @ts-expect-error/ban
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        // @ts-expect-error/ban
+        currentTime: videoRef.current.duration,
+      });
+    };
+
   }, [])
   return (
+    <>
     <section id="hero" className="noisy">
       <h1 className="title">MOJITO</h1>
       <img
@@ -73,5 +106,16 @@ export default function Hero() {
         </div>
       </div>
     </section>
+    <div className='video absolute inset-0'>
+      <video
+          src='/videos/output.mp4'
+          ref={videoRef}
+          muted
+          playsInline
+          preload='auto'
+      />
+    </div>
+
+    </>
   );
 }
