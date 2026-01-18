b7fd4b9607ab4c3abeb4110475f6b0db7ab3a7c6
Mina <lunamina246@gmail.com>
2026-01-18 08:14:54 -0500

    Implement the first part of the hero section

---
+++ cocktail-project/constants/index.d.ts
@@ -0,0 +1,6 @@
+export interface NavLink {
+  id: string;
+  title: string;
+}
+
+export declare const navLinks: NavLink[];
--- cocktail-project/src/App.tsx
+++ cocktail-project/src/App.tsx
@@ -1,17 +1,18 @@
-import gsap from 'gsap';
-import {ScrollTrigger, SplitText} from 'gsap/all'
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-
 function App() {
-  
-
   return (
-    <>
-      <p className='text-sky-800 text-2xl'>Hello React</p>
-    </>
-  )
+    <main>
+      <NavBar />
+      <Hero/>
+      <div className='h-dvh bg-black'></div>
+    </main>
+  );
 }
 
-export default App
+export default App;
